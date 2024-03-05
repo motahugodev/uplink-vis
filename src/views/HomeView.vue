6195b53a452ad26c8800dcab67569a4fe2dd5fbc
Hugo Rodrigues Mota <hugo.r.mota12@gmail.com>
2024-03-05 12:05:15 -0300

    create minimap

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,19 @@
 import { Network } from 'vis-network'
 import 'vis-network/styles/vis-network.css'
 
-import { onMounted, ref, computed } from 'vue'
+import { onMounted, ref, computed, nextTick } from 'vue'
 
 import CardComponent from '@/components/Card.js'
 import mock from '@/components/mock.js'
 
-const visRender = ref(null)
+const visRender = ref()
 const network = ref(null)
+const minimapWrapper = ref()
+const minimapImage = ref(null)
+const minimapRadar = ref(null)
+const ratio = 5
 const zoomstep = 0.5
+var targetScale
 
 const url = (item) => {
   return (
@@ -17,7 +22,7 @@ const url = (item) => {
     encodeURIComponent(
       CardComponent({
         cnpj: item.cnpj,
-        
+
         color: propsCard(
           item.entity,
           item.entityStatus,
@@ -93,22 +98,6 @@ const formatArray = () => {
   return { nodes, edges }
 }
 
-const initVis = () => {
-  const options = {
-    physics: { stabilization: false },
-    edges: { smooth: false },
-    interaction: {
-      navigationButtons: true,
-      keyboard: true
-    }
-  }
-  network.value = new Network(visRender.value, formatArray(), options)
-
-  network.value.fit('zoom', function (params) {
-    document.getElementById('selection').innerText = 'Selection: ' + params.nodes
-  })
-}
-
 const fullScreen = () => {
   if (!document.mozFullScreen && !document.webkitFullScreen) {
     if (visRender.value.mozRequestFullScreen) {
@@ -162,8 +151,125 @@ const zoomDefault = () => {
 
 const zoomPorcetage = computed(() => {
   if (!network.value) return
-  return (network.value.getScale() * 100).toFixed(0) + '%';
+  return (network.value.getScale() * 100).toFixed(0) + '%'
 })
+
+const drawMinimapWrapper = () => {
+  const { clientWidth, clientHeight } = network.value.body.container
+
+  const width = Math.round(clientWidth / ratio)
+  const height = Math.round(clientHeight / ratio)
+
+  minimapWrapper.value.style.width = `${width}px`
+  minimapWrapper.value.style.height = `${height}px`
+}
+// Draw minimap Image
+const drawMinimapImage = () => {
+  // nextTick(() => {
+  const originalCanvas = visRender.value.getElementsByTagName('canvas')[0]
+
+  const { clientWidth, clientHeight } = network.value.body.container
+
+  const tempCanvas = document.createElement('canvas')
+  const tempContext = tempCanvas.getContext('2d')
+
+  const width = Math.round((tempCanvas.width = clientWidth / ratio))
+  const height = Math.round((tempCanvas.height = clientHeight / ratio))
+
+  if (tempContext) {
+    tempContext.drawImage(originalCanvas, 0, 0, width, height)
+    minimapImage.value.src = tempCanvas.toDataURL()
+    minimapImage.value.width = width
+    minimapImage.value.height = height
+  }
+  // })
+}
+
+const drawRadar = () => {
+  const { clientWidth, clientHeight } = network.value.body.container
+  const scale = network.value.getScale()
+  const translate = network.value.getViewPosition()
+  minimapRadar.value.style.transform = `translate(${
+    (translate.x / ratio) * targetScale
+  }px, ${(translate.y / ratio) * targetScale}px) scale(${targetScale / scale})`
+  minimapRadar.value.style.width = `${clientWidth / ratio}px`
+  minimapRadar.value.style.height = `${clientHeight / ratio}px`
+}
+
+const dimMinimap = () => {
+  minimapWrapper.value.classList.remove('minimapWrapperMove')
+  minimapWrapper.value.classList.add('minimapWrapperIdle')
+}
+
+const undimMinimap = () => {
+  minimapWrapper.value.classList.remove('minimapWrapperIdle')
+  minimapWrapper.value.classList.add('minimapWrapperMove')
+}
+
+const initMinimap = () => {
+  const { clientWidth, clientHeight } = network.value.body.container
+  const width = Math.round(clientWidth / ratio)
+  const height = Math.round(clientHeight / ratio)
+  // Initial render
+
+  if (!minimapImage.value.hasAttribute('src') || minimapImage.value.src === '') {
+    if (!minimapWrapper.value.style.width || !minimapWrapper.value.style.height) {
+      drawMinimapWrapper()
+    }
+
+    setTimeout(() => {
+      drawMinimapImage()
+    }, 100)
+
+    drawRadar()
+    targetScale = network.value.view.targetScale
+  } else if (
+    minimapWrapper.value.style.width !== `${width}px` ||
+    minimapWrapper.value.style.height !== `${height}px`
+  ) {
+    minimapImage.value.removeAttribute('src')
+    drawMinimapWrapper()
+    network.value.fit()
+  } else {
+    drawRadar()
+  }
+}
+const eventsVis = () => {
+  network.value.on('resize', () => network.value.fit())
+  network.value.on('dragStart', () => undimMinimap())
+  network.value.on('dragEnd', () => dimMinimap())
+  network.value.on('zoom', () => undimMinimap())
+  network.value.on('afterDrawing', () => initMinimap())
+}
+
+const moveMinimap = (e) => {
+  if (e.buttons == 1 || e.type == 'click') {
+    e.preventDefault()
+    undimMinimap()
+    const rect = minimapWrapper.value.getBoundingClientRect()
+    const x = ((e.clientX - rect.left - rect.width / 2) * ratio) / targetScale
+    const y = ((e.clientY - rect.top - rect.height / 2) * ratio) / targetScale
+    network.value.moveTo({ position: { x, y } })
+  }
+}
+
+const initVis = async () => {
+  const options = {
+    physics: { stabilization: false },
+    edges: { smooth: false },
+    interaction: {
+      navigationButtons: true,
+      keyboard: true
+      // dragNodes: false,
+    }
+  }
+  network.value = new Network(visRender.value, formatArray(), options)
+
+  await nextTick(() => {
+    initMinimap()
+    eventsVis()
+  })
+}
 
 onMounted(() => {
   initVis()
@@ -178,7 +284,27 @@ onMounted(() => {
   <button @click="zoomOut()">Zoom Out</button>
   <button @click="zoomDefault()">Zoom Default</button>
 
-  <p id="selection"></p>
+  <div>
+    <div
+      @mousemove="moveMinimap"
+      @click="moveMinimap"
+      @mouseup="dimMinimap()"
+      ref="minimapWrapper"
+      style="
+        position: absolute;
+        margin: 5px;
+        border: 1px solid #ddd;
+        overflow: hidden;
+        background-color: #fff;
+        z-index: 9;
+      "
+      class="minimapWrapperIdle"
+    >
+      <img ref="minimapImage" class="minimapImage" />
+      <div ref="minimapRadar" class="minimapRadar"></div>
+      <!-- <img :src="teste" alt="" srcset=""> -->
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -187,5 +313,24 @@ onMounted(() => {
   height: 600px;
   border: 1px solid lightgray;
   background-color: #eeeeee;
+}
+.minimapRadar {
+  position: absolute;
+  background-color: rgba(16, 84, 154, 0.26);
+  cursor: move;
+}
+
+.minimapImage {
+  position: absolute;
+}
+
+.minimapWrapperIdle {
+  opacity: 0.2;
+  transition: opacity 0.5s;
+}
+
+.minimapWrapperMove {
+  opacity: 0.95;
+  transition: opacity 0.5s;
 }
 </style>
